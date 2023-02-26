ead216b9c6d6271ce1033225a1d8aabbe5fe0c63
ArthurMaverick <arthuracs18@gmail.com>
2023-02-26 18:56:52 -0300

    chore: Adjust outputs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "name" {
   description = "value of the name attribute of the aws_codestarconnections_connection resource"
 }
 
-output "arn" {
+output "arns" {
   value       = aws_codestarconnections_connection.this.arn
   description = "value of the arn attribute of the aws_codestarconnections_connection resource"
 }
